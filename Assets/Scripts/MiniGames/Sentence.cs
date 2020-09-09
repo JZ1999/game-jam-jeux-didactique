565a3ea278ec0dd716e9edc1c18a03d5abeb28ab
JZ1999 <joseph.zamora64@yahoo.com>
2020-09-09 03:49:45 -0600

    Added minigame 2 window to generate words from a sentence and select them

---
+++ Assets/Scripts/MiniGames/Sentence.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewSentence", menuName = "Sentence")]
+public class Sentence : ScriptableObject
+{
+	public List<string> words;
+	public List<string> badWords;
+	public AudioClip audio;
+}
---
+++ Assets/Scripts/MiniGames/WordSelect.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WordSelect : MonoBehaviour
+{
+
+	private Sentence currentSentence;
+	[SerializeField]
+	private GameObject buttonPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void setSentence(Sentence sentence)
+	{
+		List<string> tempSentences = new List<string>();
+		currentSentence = sentence;
+
+		foreach(string word in sentence.words)
+			tempSentences.Add(word);
+		foreach (string word in sentence.badWords)
+			tempSentences.Add(word);
+
+		Utils.Shuffle(tempSentences);
+
+		foreach(string word in tempSentences)
+		{
+			GameObject prefab = Instantiate(buttonPrefab, transform);
+
+			prefab.GetComponentInChildren<TextMeshProUGUI>().SetText(word);
+		}
+		
+	}
+
+}
